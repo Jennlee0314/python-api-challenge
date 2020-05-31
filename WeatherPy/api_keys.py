4f849e2a9b6737ba792a96c154d47178f25338d3
Jennlee0314 <60122752+Jennlee0314@users.noreply.github.com>
2020-05-30 21:04:27 -0400

    type commit comment here

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,46 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# OpenWeatherMap API Key\n",
+    "weather_api_key = \"a538196c2c84f4c1a8d33c1486d1f4b4\"\n",
+    "\n",
+    "# Google API Key\n",
+    "g_key = \"AIzaSyDI2IN65P80ZUaXJdczJg-QIQ5QX0G0WtI\"\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
